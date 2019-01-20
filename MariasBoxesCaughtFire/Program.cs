95f49a9833b3059210bd919ba53ff2f89112b517
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-20 17:15:25 -0500

    First Commit

---
+++ MariasBoxesCaughtFire/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MariasBoxesCaughtFire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Maria has boxes. She buys seven more. A week later, half of her boxes are destroyed in a fire, 
+            //leaving her with 22 boxes. With how many did she start?
+
+            int boxesAfterFire = 22;
+            int startingBoxes = 22 * 2;
+            Console.WriteLine("Maria started with " + startingBoxes + " boxes.");
+        }
+    }
+}
